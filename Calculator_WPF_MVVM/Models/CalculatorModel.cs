ccea71f206ffee2663358f3561fa09e153c119a4
Миша Мишин <spawn.rapture-0n@icloud.com>
2022-02-09 22:38:59 +0300

    Добавьте файлы проекта.

---
+++ Calculator_WPF_MVVM/Models/CalculatorModel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_WPF_MVVM.Models
+{
+    class CalculatorModel
+    {
+        private string result;
+
+        public string FirstOperand { get; set; }
+        public string SecondOperand { get; set; }
+        public string Operation { get; set; }
+        public string Result { get { return result; } }
+
+        private void ValidateOperand(string operand) //Проверка корректности введенных значений
+        {
+            try
+            {
+                Convert.ToDouble(operand);
+            }
+            catch (Exception)
+            {
+                result = "Введено некорректное значение: " + operand;
+            }
+        }
+
+        private void ValidateOperation(string operation) //Проверка наличия возможности выполнения операции
+        {
+            switch (operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                case "x^2":
+                case "x^-2":
+                    break;
+                default:
+                    result = "Неизвестная операция: " + operation;
+                    throw new ArgumentException("Неизвестная операция: " + operation, "operation");
+            }
+        }
+
+        private void ValidateData() //Проверка всех входящих данных
+        {
+            switch (Operation)
+            {
+                case "/":
+                case "*":
+                case "-":
+                case "+":
+                    ValidateOperand(FirstOperand);
+                    ValidateOperand(SecondOperand);
+                    break;
+                case "x^2":
+                case "x^-2":
+                    ValidateOperand(FirstOperand);
+                    break;
+                default:
+                    result = "Неизвестная операция: " + Operation;
+                    throw new ArgumentException("Неизвестная операция: " + Operation, "operation");
+            }
+        }
+
+        public void CalculateResult()
+        {
+            ValidateData();
+
+            try
+            {
+                switch (Operation)
+                {
+                    case "+":
+                        result = (Convert.ToDouble(FirstOperand) + Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case "-":
+                        result = (Convert.ToDouble(FirstOperand) - Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case "*":
+                        result = (Convert.ToDouble(FirstOperand) * Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+
+                    case "/":
+                        result = (Convert.ToDouble(FirstOperand) / Convert.ToDouble(SecondOperand)).ToString();
+                        break;
+                    case "x^2":
+                        result = Math.Pow(Convert.ToDouble(FirstOperand), 2).ToString();
+                        break;
+                    case "x^-2":
+                        if (Convert.ToDouble(FirstOperand) < 0)
+                        {
+                            result = "Попытка извлечения корня из отрицательного числа.";
+                        }
+                        else
+                        {
+                            result = Math.Sqrt(Convert.ToDouble(FirstOperand)).ToString();
+                        }
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                result = "!Ошибка. Неизвестная операция.";
+            }
+        }
+
+        public CalculatorModel(string firstOperand, string secondOperand, string operation)
+        {
+            ValidateOperand(firstOperand);
+            ValidateOperand(secondOperand);
+            ValidateOperation(operation);
+
+            FirstOperand = firstOperand;
+            SecondOperand = secondOperand;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public CalculatorModel(string firstOperand, string operation)
+        {
+            ValidateOperand(firstOperand);
+            ValidateOperation(operation);
+
+            FirstOperand = firstOperand;
+            SecondOperand = string.Empty;
+            Operation = operation;
+            result = string.Empty;
+        }
+
+        public CalculatorModel()
+        {
+            FirstOperand = string.Empty;
+            SecondOperand = string.Empty;
+            Operation = string.Empty;
+            result = string.Empty;
+        }
+    }
+}
---
+++ Calculator_WPF_MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,188 @@
+﻿using Calculator_WPF_MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator_WPF_MVVM.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string ProprtyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(ProprtyName));
+        }
+
+        private CalculatorModel calculation;
+
+        private string lastOperation;
+        private bool newDisplayRequired = false;
+        private string display;
+
+        public MainWindowViewModel()
+        {
+            this.calculation = new CalculatorModel();
+            this.display = "0";
+            this.FirstOperand = string.Empty;
+            this.SecondOperand = string.Empty;
+            this.Operation = string.Empty;
+            this.lastOperation = string.Empty;
+            OperationButtonPressCommand = new RelayCommand(OnOperationButtonPressExecute, CanOperationButtonPressCommandExecuted);
+            SingularOperationButtonPressCommand = new RelayCommand(OnSingularOperationButtonPressCommandExecute, CanSingularOperationButtonPressCommandExecuted);
+            SymbolButtonPressCommand = new RelayCommand(OnSymbolButtonPressCommandExecute, null);
+        }
+
+        public string FirstOperand
+        {
+            get { return calculation.FirstOperand; }
+            set { calculation.FirstOperand = value; }
+        }
+
+        public string SecondOperand
+        {
+            get { return calculation.SecondOperand; }
+            set { calculation.SecondOperand = value; }
+        }
+
+        public string Operation
+        {
+            get { return calculation.Operation; }
+            set { calculation.Operation = value; }
+        }
+
+        public string LastOperation
+        {
+            get { return lastOperation; }
+            set { lastOperation = value; }
+        }
+
+        public string Result
+        {
+            get { return calculation.Result; }
+        }
+
+        public string Display
+        {
+            get { return display; }
+            set
+            {
+                display = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SymbolButtonPressCommand { get; }
+        public void OnSymbolButtonPressCommandExecute(object button)
+        {
+            switch (button)
+            {
+                case "C":
+                    Display = "0";
+                    FirstOperand = string.Empty;
+                    SecondOperand = string.Empty;
+                    Operation = string.Empty;
+                    LastOperation = string.Empty;
+                    break;
+                case "DEL":
+                    if (display.Length > 1)
+                        Display = display.Substring(0, display.Length - 1);
+                    else Display = "0";
+                    break;
+                case ".":
+                    if (newDisplayRequired)
+                    {
+                        Display = "0,";
+                    }
+                    else
+                    {
+                        if (!display.Contains("."))
+                        {
+                            Display = display + ",";
+                        }
+                    }
+                    break;
+                default:
+                    if (display == "0" || newDisplayRequired)
+                        Display = (string)button;
+                    else
+                        Display = display + (string)button;
+                    break;
+            }
+            newDisplayRequired = false;
+        }
+
+        public ICommand OperationButtonPressCommand { get; }
+        public void OnOperationButtonPressExecute(object operation)
+        {
+            try
+            {
+                if (FirstOperand == string.Empty || LastOperation == "=")
+                {
+                    FirstOperand = display;
+                    LastOperation = operation as string;
+                }
+                else
+                {
+                    SecondOperand = display;
+                    Operation = lastOperation;
+                    calculation.CalculateResult();
+                    LastOperation = operation as string;
+                    Display = Result;
+                    FirstOperand = display;
+                }
+                newDisplayRequired = true;
+            }
+            catch (Exception ex)
+            {
+                Display = $"Ошибка: {ex.Message}";
+            }
+        }
+        public bool CanOperationButtonPressCommandExecuted(object operation)
+        {
+            if ((string)operation == "/" && Display == "0")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public ICommand SingularOperationButtonPressCommand { get; }
+        public void OnSingularOperationButtonPressCommandExecute(object operation)
+        {
+            try
+            {
+                FirstOperand = Display;
+                Operation = (string)operation;
+                calculation.CalculateResult();
+                LastOperation = "=";
+                Display = Result;
+                FirstOperand = display;
+                newDisplayRequired = true;
+            }
+            catch (Exception ex)
+            {
+                Display = $"Ошибка: {ex.Message}";
+            }
+        }
+        public bool CanSingularOperationButtonPressCommandExecuted(object operation)
+        {
+            if ((string)operation == "x^-2" && Convert.ToDouble(Display) < Convert.ToDouble(0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
